b72cc3241c3f23a06df97dd5bf41c85e0ad5575f
jahnaviio <jahnaviduggapu1901@gmail.com>
2026-03-22 00:42:30 +0530

    Adjust image scaling in profile component
    update profile

--- src/App.tsx
+++ src/App.tsx
@@ -143,7 +143,7 @@ const Hero = () => {
            <img 
              src={myphoto} 
                alt="profile"
-                className="w-full h-full rounded-full object-cover object-top scale-80"
+                className="w-full h-full rounded-full object-cover object-top scale-100"
                   />
             </div>
           </div>
